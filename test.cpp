14f1eb2675fa04f42f1850eaa7044ffd3d47a30f
wrzontek <matwiejuk.adrian@gmail.com>
2020-11-19 14:40:45 +0100

    początek

--- test.cpp
+++ test.cpp
@@ -7,8 +7,8 @@ int main() {
     cout << p1.x() << " " << p1.y() << endl;
     Position p2 = p1.reflection();
     cout << p2.x() << " " << p2.y() << endl;
-    const Position p3 = Position::origin();
-    p3 = p1.reflection();
+    Position p3 = Position::origin(); // powinien dawać const
+    p3 = p1.reflection(); // źle że działa
     //Position p2 = Position() // nie działa i ma nie działać, git
     return 0;
 }
